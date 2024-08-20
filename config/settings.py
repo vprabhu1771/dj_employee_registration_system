99e0b3e60574a692103bc7b122d9685f579b1cdb
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-20 08:33:41 +0530

    rest framework auth tokken added to settings.py

--- config/settings.py
+++ config/settings.py
@@ -146,3 +146,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = 'media/'
 
 AUTH_USER_MODEL = 'backend.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
