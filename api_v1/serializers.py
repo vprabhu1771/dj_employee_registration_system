4d32044e3c1d7d061b2409b02aca7a88b2908b35
Prabhu <32477279+vprabhu1771@users.noreply.github.com>
2024-08-20 09:30:26 +0530

    api v1 Customer User CRUD also known as Employee CRUD

--- api_v1/serializers.py
+++ api_v1/serializers.py
@@ -2,6 +2,26 @@ from rest_framework import serializers
 from backend.models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'first_name', 'last_name', 'email', 'gender', 'image', 'phone', 'hire_date']
+        fields = [
+            'first_name',
+            'last_name',
+            'email'
+            'gender',
+            'password',
+            'phone',
+            'hire_date'
+        ]
+
+    def create(self, validated_data):
+        user = super(CustomUserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        # group = Group.objects.get(name='EMPLOYEE')
+        # user.groups.add(group)
+        user.is_staff = True
+        user.is_active = True
+        user.save()
+        return user
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from backend.models import CustomUser
+from .serializers import CustomUserSerializer
+from rest_framework.permissions import AllowAny
+
 
 # Create your views here.
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [AllowAny]
