0cecb63cf9a75f2acc9519008c76f57665c6fcd7
vprabhu1771 <vprabhu1771@gmail.com>
2024-08-21 12:47:59 +0530

    quryset issue fixed

--- api_v1/views.py
+++ api_v1/views.py
@@ -4,6 +4,9 @@ from backend.models import CustomUser
 from .serializers import CustomUserSerializer
 from rest_framework.permissions import AllowAny
 
+from django.contrib.auth.models import Group
+from rest_framework.response import Response
+
 
 # Create your views here.
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -11,13 +14,22 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     permission_classes = [AllowAny]
 
-    def get_queryset(self):
+    def list(self, request, *args, **kwargs):
         # Get the group instance for 'EMPLOYEE'
         try:
             employees_group = Group.objects.get(name='EMPLOYEE')
         except Group.DoesNotExist:
-            # Return an empty queryset if the group does not exist
-            return CustomUser.objects.none()
+            # Return an empty response if the group does not exist
+            return Response({'data': []})
 
         # Filter users by the 'EMPLOYEE' group
-        return CustomUser.objects.filter(groups=employees_group)
+        queryset = CustomUser.objects.filter(groups=employees_group)
+
+        # Serialize the queryset
+        serializer = self.get_serializer(queryset, many=True)
+
+        # Return the response with custom data structure
+        data = {
+            'data': serializer.data
+        }
+        return Response(data)
