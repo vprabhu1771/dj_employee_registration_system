caa7882622daf17d7440c8dd1532d624d8f44222
vprabhu1771 <vprabhu1771@gmail.com>
2024-08-25 18:41:37 +0530

    api v1 login obtain_auth_token import fixed

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CustomUserViewSet
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 router = DefaultRouter()
 router.register(r'employees', CustomUserViewSet, basename='employees')
 
