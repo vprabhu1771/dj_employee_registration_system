ec4e87e740bbcc16dabfd411e1eee40d07473b81
vprabhu1771 <vprabhu1771@gmail.com>
2024-08-25 18:43:34 +0530

    api v1 login views import fixed

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from backend.models import CustomUser
-from .serializers import CustomUserSerializer
+from .serializers import CustomUserSerializer, EmailAuthTokenSerializer
 from rest_framework.permissions import AllowAny
 
 from django.contrib.auth.models import Group
 from rest_framework.response import Response
 
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
 class CustomUserViewSet(viewsets.ModelViewSet):
